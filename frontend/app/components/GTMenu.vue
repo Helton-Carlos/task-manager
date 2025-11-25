7ef3327e07ac6e8a3b890bd711f5907c1eacc652
Helton Silva <helton.silva@melhorenvio.com>
2025-11-25 08:44:18 -0300

    fix: adjust menu style

--- frontend/app/components/GTMenu.vue
+++ frontend/app/components/GTMenu.vue
@@ -18,35 +18,39 @@ const menu = ref<{ icon: string; label: string; path: string }[]>([
   {
     icon: "lucide:settings",
     label: "Configurações",
-    path: ''
+    path: "",
   },
 ]);
 </script>
 
 <template>
-    <div class="w-[250px] h-screen flex flex-col">
-     <div class="mb-8 flex justify-start">
-       <h1 class="bg-blue-700 font-bold text-2xl text-white px-2 py-1 mx-auto my-8 round-md">CRM</h1>
-     </div>
-   
-      <nav class="container flex-1">
-        <ul class="flex flex-col justify-center gap-8 mx-4">
-          <li 
-            v-for="item in menu" :key="item.path"
-            class="text-sm text-gray-500 hover:text-blue-500 font-bold flex items-center gap-2"
-          > 
-            <Icon :name="item.icon" size="24" color="blue" /> 
-            <span class="text-sm">{{ item.label }}</span>
-          </li>
-        </ul>
-      </nav>
-
-      <GTButton
-        class="flex justify-end items-center gap-4 mx-auto my-8"
-        type="button"
-        underline
+  <div class="w-[250px] h-screen flex flex-col">
+    <div class="mb-8 flex justify-start">
+      <h1
+        class="bg-blue-700 font-bold text-2xl text-white px-2 py-1 mx-auto my-8 round-md"
       >
-        <span class="text-gray-500 hover:underline">Sair</span>
-      </GTButton>
-   </div>
+        CRM
+      </h1>
+    </div>
+
+    <nav class="container flex-1">
+      <ul class="flex flex-col justify-center gap-8 mx-4">
+        <li
+          v-for="item in menu"
+          :key="item.path"
+          class="text-sm text-gray-500 hover:text-blue-500 font-bold flex items-center gap-2 cursor-pointer"
+        >
+          <Icon :name="item.icon" size="24" color="blue" />
+          <span class="text-sm">{{ item.label }}</span>
+        </li>
+      </ul>
+    </nav>
+
+    <NuxtLink
+      class="flex justify-end items-center gap-4 mx-auto my-8"
+      to="/login"
+    >
+      <span class="text-gray-500 font-bold hover:underline">Sair</span>
+    </NuxtLink>
+  </div>
 </template>
