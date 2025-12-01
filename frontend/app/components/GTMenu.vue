780d44a7440f966a38fddb9229bb3b3d18b98a3d
Helton Silva <helton.silva@melhorenvio.com>
2025-12-01 14:48:19 -0300

    feat: add menu mobile

--- frontend/app/components/GTMenu.vue
+++ frontend/app/components/GTMenu.vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+import { useBreakpoints } from "~/composables/useBreakpoints";
+
+const { sm } = useBreakpoints();
+const openMenu = ref<boolean>(false);
+
 const menu = ref<{ icon: string; label: string; path: string }[]>([
   {
     icon: "lucide:layout-dashboard",
@@ -24,34 +29,84 @@ const menu = ref<{ icon: string; label: string; path: string }[]>([
 </script>
 
 <template>
-  <div class="w-[250px] h-screen bg-white flex flex-col shadow">
-    <div class="mb-8 flex justify-start">
-      <h1
-        class="bg-blue-700 font-bold text-2xl text-white px-2 py-1 mx-auto my-5 round-md"
+  <div>
+    <div v-if="sm" class="menu-desktop">
+      <div class="mb-8 flex justify-start">
+        <h1
+          class="bg-blue-700 font-bold text-2xl text-white px-2 py-1 mx-auto my-5 round-md"
+        >
+          CRM
+        </h1>
+      </div>
+
+      <nav class="container flex-1 mt-8">
+        <ul class="flex flex-col justify-center gap-8 mx-4">
+          <NuxtLink
+            v-for="item in menu"
+            :key="item.path"
+            :to="item.path"
+            class="text-sm text-gray-500 hover:text-blue-500 font-bold flex items-center gap-2 cursor-pointer"
+          >
+            <Icon :name="item.icon" size="24" color="blue" />
+            <span class="text-sm">{{ item.label }}</span>
+          </NuxtLink>
+        </ul>
+      </nav>
+
+      <NuxtLink
+        class="flex justify-end items-center gap-4 mx-auto my-8"
+        to="/login"
       >
-        CRM
-      </h1>
+        <span class="text-gray-500 font-bold hover:underline">Sair</span>
+      </NuxtLink>
     </div>
 
-    <nav class="container flex-1 mt-8">
-      <ul class="flex flex-col justify-center gap-8 mx-4">
-        <NuxtLink
-          v-for="item in menu"
-          :key="item.path"
-          :to="item.path"
-          class="text-sm text-gray-500 hover:text-blue-500 font-bold flex items-center gap-2 cursor-pointer"
-        >
-          <Icon :name="item.icon" size="24" color="blue" />
-          <span class="text-sm">{{ item.label }}</span>
-        </NuxtLink>
-      </ul>
-    </nav>
+    <div v-else class="menu-mobile">
+      <Icon
+        name="lucide:menu"
+        size="20"
+        color="blue"
+        class="cursor-pointer"
+        @click="() => (openMenu = !openMenu)"
+      />
+    </div>
 
-    <NuxtLink
-      class="flex justify-end items-center gap-4 mx-auto my-8"
-      to="/login"
+    <div
+      v-if="openMenu"
+      class="absolute top-0 bg-white w-screen h-screen !z-50 p-4 flex flex-col"
     >
-      <span class="text-gray-500 font-bold hover:underline">Sair</span>
-    </NuxtLink>
+      <div class="flex justify-end mb-8">
+        <Icon
+          name="lucide:x"
+          size="24"
+          color="blue"
+          class="cursor-pointer"
+          @click="() => (openMenu = false)"
+        />
+      </div>
+
+      <nav class="container flex justify-center mt-4 flex-1">
+        <ul class="flex flex-col justify-center gap-8 mx-4">
+          <NuxtLink
+            v-for="item in menu"
+            :key="item.path"
+            :to="item.path"
+            class="text-sm text-gray-500 hover:text-blue-500 font-bold flex items-center gap-2 cursor-pointer"
+            @click="() => (openMenu = false)"
+          >
+            <Icon :name="item.icon" size="24" color="blue" />
+            <span class="text-sm">{{ item.label }}</span>
+          </NuxtLink>
+        </ul>
+      </nav>
+
+      <NuxtLink
+        class="flex justify-end items-center gap-4 mx-auto my-8"
+        to="/login"
+        @click="() => (openMenu = false)"
+      >
+        <span class="text-gray-500 font-bold hover:underline">Sair</span>
+      </NuxtLink>
+    </div>
   </div>
 </template>
